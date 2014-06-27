d1dcd48016d5715f684dae36e237574bb61e293a
oldcap <zhe.zhang.research@gmail.com>
2014-06-27 11:02:01 -0400

    buf size

--- inode-locking.c
+++ inode-locking.c
@@ -11,13 +11,13 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	char buf[10240];
+	char buf[12288];
 
 	int fd = open(argv[1], O_RDWR | O_DIRECT);
 
 	while(1) {
 		sleep(1);
-		read(fd, buf, 10240);
+		read(fd, buf, 12288);
 		printf("%s\n", buf + 10200);
 	}
 
