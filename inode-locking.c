904d08eb23289da4dd30ac16927b5333955d5757
Zhe Zhang <zhezhang@cloudera.com>
2014-09-12 21:56:19 -0700

    seeking

--- inode-locking.c
+++ inode-locking.c
@@ -9,17 +9,26 @@
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
-		printf("Usage: inode-locking <file name>\n");
+		printf("Usage: inode-locking <file name> <offset>\n");
 		return -1;
 	}
 
 	char * buf = memalign(4096, BUF_SIZE);
 
 	// int fd = open(argv[1], O_RDWR | O_DIRECT);
-	int fd = open(argv[1], O_RDWR | O_DIRECT);
+	int fd = open(argv[1], O_RDWR);
+	unsigned long long offset;
+	if (argc > 2) {
+		offset = strtoull(argv[2], NULL, 10);
+	} else {
+		offset = 0;
+	}
 
 	while(1) {
 		sleep(1);
+		if (lseek(fd, offset, SEEK_SET) != offset) {
+			printf("Cannot seek\n");
+		}
 		read(fd, buf, BUF_SIZE);
 		printf("%s\n", buf + (BUF_SIZE - 128));
 	}
